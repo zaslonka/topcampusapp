c2ab34aec36462886d42e625ea0c60dbe787c9fe
zaslonka <zaslonka@gmail.com>
2025-05-31 08:50:13 +0200

    Removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WelcomeBanner from "./components/WelcomeBanner";
 import { ConfigProvider } from "./context/ConfigContext";
